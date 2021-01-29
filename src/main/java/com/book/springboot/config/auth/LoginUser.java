61550ebe84026157fb2710b47d096970dcad6135
kimjaeyeol <kjy@solutis.co.kr>
2021-01-29 17:40:44 +0900

    LoginUser 어노테이션 객체 생성

--- src/main/java/com/book/springboot/config/auth/LoginUser.java
+++ src/main/java/com/book/springboot/config/auth/LoginUser.java
@@ -1,4 +1,12 @@
 package com.book.springboot.config.auth;
 
-public class LoginUser {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER) // 메소드의 파라미터로 선언된 객체에서 사용할 수 있도록 설정
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoginUser {
+    // @interface : 어노테이션 클래스로 지정, LoginUser라는 이름을 가진 어노테이션이 생성되었다고 보면 된다
 }
--- src/main/java/com/book/springboot/config/auth/LoginUserArgumentResolver.java
+++ src/main/java/com/book/springboot/config/auth/LoginUserArgumentResolver.java
@@ -1,4 +1,33 @@
 package com.book.springboot.config.auth;
 
-public class LoginUserArgumentResolver {
+import com.book.springboot.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+    
+    private final HttpSession httpSession;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
+        boolean isUserClass = SessionUser.class.equals(parameter.getParameterType());
+        // 파라미터에 @LoginUser 어노테이션이 붙어 있고, 파라미터 클래스 타입이 SessionUser.class인 경우 true
+        return isLoginUserAnnotation && isUserClass;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        // 파라미터에 전달할 객체를 생성
+        return httpSession.getAttribute("user");
+    }
 }
