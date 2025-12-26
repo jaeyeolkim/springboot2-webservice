6b7f5e987e1c1cc24d852530eb86a44ced6e620c
jaeyeolkim <dev_1@becuai.com>
2025-12-26 15:08:00 +0900

    ⬆️ 최신 코드로 변경하여 오류 없이 로컬에서 실행되는 버전(oauth 주석)

--- src/main/java/com/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/book/springboot/config/auth/SecurityConfig.java
@@ -24,10 +24,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated() // 나머지 URL들은 모두 로그인한 사용자에게 허용
             .and()
                 .logout()
-                    .logoutSuccessUrl("/")
-            .and()
-                .oauth2Login() // OAuth 2 로그인 기능 설정의 진입점
-                    .userInfoEndpoint() // OAuth 2 로그인 성공 이후 사용자 정보를 가져올 때의 설정
-                        .userService(customOAuth2UserService); // 로그인 성공 시 후속 조치를 진행할 UserService 인터페이스의 구현체를 등록, 사용자 정보를 가져온 상태에서 추가로 진행하고자 하는 기능을 명시
+                    .logoutSuccessUrl("/");
+//            .and()
+//                .oauth2Login() // OAuth 2 로그인 기능 설정의 진입점
+//                    .userInfoEndpoint() // OAuth 2 로그인 성공 이후 사용자 정보를 가져올 때의 설정
+//                        .userService(customOAuth2UserService); // 로그인 성공 시 후속 조치를 진행할 UserService 인터페이스의 구현체를 등록, 사용자 정보를 가져온 상태에서 추가로 진행하고자 하는 기능을 명시
     }
 }
--- src/test/java/com/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/book/springboot/web/HelloControllerTest.java
@@ -1,21 +1,19 @@
 package com.book.springboot.web;
 
 import com.book.springboot.config.auth.SecurityConfig;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * RunWith(SpringRunner.class) : 스프링 부트 테스트와 JUnit 사이에 연결자 역할을 합니다.
@@ -23,7 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * SecurityConfig는 읽었지만, SecurityConfig를 생성하기 위해 필요한 CustomOAuth2UserService는 읽을수가 없어 오류가 발생하다.
  * 따라서, 스캔 대상에서 SecurityConfig를 제거한다.
  */
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = HelloController.class,
         excludeFilters = {
                 @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
